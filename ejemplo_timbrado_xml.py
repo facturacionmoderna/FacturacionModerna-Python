25bdf4efc09296527cee6389bdd36af12272a805
root <root@debian2.oax2>
2013-12-10 13:59:58 -0600

    Se agregaron las referencias para timbrado de recibos de nomina y correcion al sellado del XML

--- ejemplo_timbrado_xml.py
+++ ejemplo_timbrado_xml.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import base64
 from M2Crypto import RSA
 from lxml import etree as ET
-import sha
+import hashlib
 import os
 
 def prueba_timbrado(debug = False):
@@ -51,21 +51,23 @@ def sella_xml(cfdi, numero_certificado, archivo_cer, archivo_pem):
   xsl_root = ET.parse('utilerias/xslt32/cadenaoriginal_3_2.xslt')
   xsl = ET.XSLT(xsl_root)
   cadena_original = xsl(xdoc)
-  digest = sha.new(str(cadena_original))
-  sello = base64.b64encode(keys.sign(digest.hexdigest()))
+  digest = hashlib.new('sha1', str(cadena_original)).digest()
+  sello = base64.b64encode(keys.sign(digest, "sha1"))
+
 
   comp = xdoc.get('Comprobante')
   xdoc.attrib['sello'] = sello
   xdoc.attrib['certificado'] = cert
   xdoc.attrib['noCertificado'] = numero_certificado
+  print ET.tostring(xdoc)
   return ET.tostring(xdoc)
 
 def genera_xml(rfc_emisor):
   # se calcula la fecha de emisión en formato ISO 8601
-  fecha_actual = datetime.now().isoformat()
+  fecha_actual = str(datetime.now().isoformat())[:19]
   
   cfdi = """<?xml version="1.0" encoding="UTF-8"?>
-<cfdi:Comprobante xsi:schemaLocation="http://www.sat.gob.mx/cfd/3 http://www.sat.gob.mx/sitio_internet/cfd/3/cfdv32.xsd" xmlns:cfdi="http://www.sat.gob.mx/cfd/3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" version="3.2" fecha="{fecha_actual}" tipoDeComprobante="ingreso" noCertificado="" certificado="" sello="" formaDePago="Pago en una sola exhibición" metodoDePago="Transferencia Electrónica" NumCtaPago="No identificado" LugarExpedicion="San Pedro Garza García, Mty." subTotal="10.00" total="11.60">
+<cfdi:Comprobante xsi:schemaLocation="http://www.sat.gob.mx/cfd/3 http://www.sat.gob.mx/sitio_internet/cfd/3/cfdv32.xsd" xmlns:cfdi="http://www.sat.gob.mx/cfd/3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" version="3.2" fecha="{fecha_actual}" tipoDeComprobante="ingreso" noCertificado="" certificado="" sello="" formaDePago="Pago en una sola exhibicion" metodoDePago="Transferencia Electronica" NumCtaPago="No identificado" LugarExpedicion="San Pedro Garza Garcia, Mty." subTotal="10.00" total="11.60">
 <cfdi:Emisor nombre="EMPRESA DEMO" rfc="{rfc_emisor}">
   <cfdi:RegimenFiscal Regimen="No aplica"/>
 </cfdi:Emisor>
