103bf0308102effab97e2f65ea1d6810fce0ee40
devlara2 <desarrollo@facturacionmoderna.com>
2017-10-26 12:12:24 -0400

    se actualiza ejemplo layout 3.3

--- ejemplo_timbrado_layout_33.py
+++ ejemplo_timbrado_layout_33.py
@@ -41,47 +41,64 @@ def genera_layout(rfc_emisor):
   
   cfdi = """[ComprobanteFiscalDigital]
 
-;Version=3.3
+Version=3.3
 Serie=A
-Folio=asignarFolio
+Folio=02
 Fecha={fecha_actual}
-FormaPago=99
-NoCertificado=20001000000300022762
+FormaPago=03
+NoCertificado=20001000000200000192
 CondicionesDePago=CONTADO
-SubTotal=100.00
-Descuento=00.00
+SubTotal=1850
+Descuento=175.00
 Moneda=MXN
-;TipoCambio=
-Total=100.00
+Total=1943.00
 TipoDeComprobante=I
 MetodoPago=PUE
 LugarExpedicion=68050
 
 [DatosAdicionales]
 tipoDocumento=FACTURA
+observaciones=Observaciones al documento versión 3.3
+plantillaPDF=clasic
+logotipo= lg_fdc9422f0756154cd565695
 
 [Emisor]
-Rfc={rfc_emisor}
+Rfc=ESI920427886
 Nombre=FACTURACION MODERNA SA DE CV
 RegimenFiscal=601
 
 [Receptor]
 Rfc=XAXX010101000
-Nombre=LUIS HERNANDEZ FELIX
+Nombre=PUBLICO EN GENERAL
 UsoCFDI=G01
 
 
 [Concepto#1]
 ClaveProdServ=01010101
-NoIdentificacion=
-Cantidad=10
-ClaveUnidad=KGM
-Unidad=Kilogramos
-Descripcion=AZUCAR
-ValorUnitario=10.00
-Importe=100.00
-Descuento=00.00
+NoIdentificacion=AULOG001
+Cantidad=5
+ClaveUnidad=H87
+Unidad=Pieza
+Descripcion=Aurriculares USB Logitech
+ValorUnitario=350.00
+Importe=1750.00
+Descuento=175.00
 
+Impuestos.Traslados.Base=[1575.00]
+Impuestos.Traslados.Impuesto=[002]
+Impuestos.Traslados.TipoFactor=[Tasa]
+Impuestos.Traslados.TasaOCuota=[0.160000]
+Impuestos.Traslados.Importe=[252.00]
+
+[Concepto#2]
+ClaveProdServ=43201800
+NoIdentificacion=USB
+Cantidad=1
+ClaveUnidad=H87
+Unidad=Pieza
+Descripcion=Memoria USB 32gb marca Kingston
+ValorUnitario=100.00
+Importe=100.00
 
 Impuestos.Traslados.Base=[100.00]
 Impuestos.Traslados.Impuesto=[002]
@@ -89,23 +106,13 @@ Impuestos.Traslados.TipoFactor=[Tasa]
 Impuestos.Traslados.TasaOCuota=[0.160000]
 Impuestos.Traslados.Importe=[16.00]
 
-Impuestos.Retenciones.Base=[100.00]
-Impuestos.Retenciones.Impuesto=[001]
-Impuestos.Retenciones.TipoFactor=[Tasa]
-Impuestos.Retenciones.TasaOCuota=[0.35]
-Impuestos.Retenciones.Importe=[35.00]
+[Traslados]
+TotalImpuestosTrasladados=268.00
+Impuesto=[002]
+TipoFactor=[Tasa]
+TasaOCuota=[0.160000]
+Importe=[268.00]
 
-
-
-[Traslado#1]
-Impuesto = 002
-TipoFactor = Tasa
-TasaOCuota = 0.160000
-Importe = 16.00
-
-[Retencion#1]
-Impuesto=001
-Importe=15.99
 """.format(**locals())
   return cfdi;
 
